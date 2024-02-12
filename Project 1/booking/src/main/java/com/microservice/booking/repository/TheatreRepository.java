ae3de7d52b825f81a01deb6b4aa5f588443c0ad1
Shweta Shukla <krianu20214@gmail.com>
2024-02-12 22:57:48 +0530

    fixed code

--- Project 1/booking/src/main/java/com/microservice/booking/repository/TheatreRepository.java
+++ Project 1/booking/src/main/java/com/microservice/booking/repository/TheatreRepository.java
@@ -1,6 +1,7 @@
 package com.microservice.booking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.microservice.booking.entities.Theatre;
@@ -8,5 +9,8 @@ import com.microservice.booking.entities.Theatre;
 
 @Repository
 public interface TheatreRepository extends JpaRepository<Theatre, Long> {
+
+	@Query(value = "SELECT * FROM theatre t WHERE t.theatre_id =:theatre_id",nativeQuery = true)
+	Theatre getTheatrebyId(Long theatre_id);
 
 }
