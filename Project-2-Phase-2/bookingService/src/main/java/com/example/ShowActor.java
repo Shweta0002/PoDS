143ab7bc38ce3c65f3cb5145982025e20d209e02
Bhavya Choudhary <bhavyachoudhary.30@gmail.com>
2024-04-30 23:09:31 +0530

    Fixes

--- Project-2-Phase-2/bookingService/src/main/java/com/example/ShowActor.java
+++ Project-2-Phase-2/bookingService/src/main/java/com/example/ShowActor.java
@@ -13,91 +13,113 @@ import akka.cluster.sharding.typed.javadsl.EntityTypeKey;
 import akka.cluster.sharding.typed.javadsl.ClusterSharding;
 import akka.cluster.sharding.typed.javadsl.EntityRef;
 
-
 import java.util.*;
 
 import com.example.CborSerializable;
 
-public class ShowActor extends AbstractBehavior<ShowActor.Command>  {
+public class ShowActor extends AbstractBehavior<ShowActor.Command> {
 
     public Integer id;
     public Integer theatre_id;
     String title;
     public Integer price;
     public Integer seats_available;
-    public static Integer count=0;
-    public final List<ShowActor.Booking> bookings=new ArrayList<>();
-    public static final EntityTypeKey<ShowActor.Command> TypeKey =
-			    EntityTypeKey.create(ShowActor.Command.class,"SimpleCounterEntity");
-	
+    public static Integer count = 0;
+    public final List<ShowActor.Booking> bookings = new ArrayList<>();
+    public static final EntityTypeKey<ShowActor.Command> TypeKey = EntityTypeKey.create(ShowActor.Command.class,
+            "SimpleCounterEntity");
+
     // actor protocol
-    sealed interface Command extends CborSerializable{}
-
-    public final record Show(Integer id, Integer theatre_id , String title,Integer price ,Integer seats_available)implements CborSerializable{}
-    public final record Booking(Integer id,Integer user_id,Integer show_id , Integer seats_booked)implements CborSerializable{}
-    public final record Bookings(List<ShowActor.Booking> bookings)implements CborSerializable{}
-    public final record Response(boolean flag)implements CborSerializable{}
-
-    public final record GetShow(ActorRef<Show> replyTo) implements Command{}
-    public final record AddBooking(Booking booking,ActorRef<ShowActor.Booking> replyTo) implements Command{}
-    public final record DeleteUserBooking(Integer user_id,Integer show_id, ActorRef<Response> replyTo, ActorRef<WorkerActor.Command> workerReply) implements Command{}
-
-    public final record DeleteAllBookings(ActorRef<WorkerActor.Command> workerReply) implements Command{}
-    public final record GetBookings(ActorRef<WorkerActor.Command> workerRef) implements Command{}
-   // private final Http http;
-    public final record GetTheatre(ActorRef<WorkerActor.Command> workerRef,Integer i, Integer theatre_id,ActorRef<WorkerActor.Showlist> replyTo ) implements Command{}
-    private ShowActor(ActorContext<Command> context,Integer id,Integer theatre_id, String title , Integer price,Integer seats_available) {
-        super(context);
-        this.id=id;
-        this.theatre_id=theatre_id;
-        this.title=title;
-        this.price=price;
-        this.seats_available=seats_available;
+    sealed interface Command extends CborSerializable {
     }
 
-    public static Behavior<Command> create(Integer id,Integer theatre_id, String title , Integer price,Integer seats_available) {
-        return Behaviors.setup(context->new ShowActor(context,id,theatre_id,title ,price,seats_available));
+    public final record Show(Integer id, Integer theatre_id, String title, Integer price, Integer seats_available)
+            implements CborSerializable {
+    }
+
+    public final record Booking(Integer id, Integer user_id, Integer show_id, Integer seats_booked)
+            implements CborSerializable {
+    }
+
+    public final record Bookings(List<ShowActor.Booking> bookings) implements CborSerializable {
+    }
+
+    public final record Response(boolean flag) implements CborSerializable {
+    }
+
+    public final record GetShow(ActorRef<Show> replyTo) implements Command {
+    }
+
+    public final record AddBooking(Booking booking, ActorRef<ShowActor.Booking> replyTo) implements Command {
+    }
+
+    public final record DeleteUserBooking(Integer user_id, Integer show_id, ActorRef<Response> replyTo,
+            ActorRef<WorkerActor.Command> workerReply) implements Command {
+    }
+
+    public final record DeleteAllBookings(ActorRef<WorkerActor.Command> workerReply) implements Command {
+    }
+
+    public final record GetBookings(ActorRef<WorkerActor.Command> workerRef) implements Command {
+    }
+
+    // private final Http http;
+    public final record GetTheatre(ActorRef<WorkerActor.Command> workerRef, Integer i, Integer theatre_id,
+            ActorRef<WorkerActor.Showlist> replyTo) implements Command {
+    }
+
+    private ShowActor(ActorContext<Command> context, Integer id, Integer theatre_id, String title, Integer price,
+            Integer seats_available) {
+        super(context);
+        this.id = id;
+        this.theatre_id = theatre_id;
+        this.title = title;
+        this.price = price;
+        this.seats_available = seats_available;
+    }
+
+    public static Behavior<Command> create(Integer id, Integer theatre_id, String title, Integer price,
+            Integer seats_available) {
+        return Behaviors.setup(context -> new ShowActor(context, id, theatre_id, title, price, seats_available));
     }
 
     @Override
     public Receive<Command> createReceive() {
         return newReceiveBuilder()
                 .onMessage(GetShow.class, this::onGetShow)
-                .onMessage(AddBooking.class,this::onAddBooking)
-                .onMessage(DeleteUserBooking.class,this::onDeleteUserBooking)
-                .onMessage(GetBookings.class,this::onGetBookings)
-                .onMessage(GetTheatre.class,this::onGetTheatre)
+                .onMessage(AddBooking.class, this::onAddBooking)
+                .onMessage(DeleteUserBooking.class, this::onDeleteUserBooking)
+                .onMessage(GetBookings.class, this::onGetBookings)
+                .onMessage(GetTheatre.class, this::onGetTheatre)
                 .onMessage(DeleteAllBookings.class, this::onDeleteAllBookings)
                 .build();
     }
 
     private Behavior<Command> onGetShow(GetShow message) {
         getContext().getLog().info("Show details recv");
-        getContext().getLog().info("Show details to "+ message.replyTo());
-        message.replyTo().tell(new Show(this.id,this.theatre_id,this.title,this.price,this.seats_available));
+        getContext().getLog().info("Show details to " + message.replyTo());
+        message.replyTo().tell(new Show(this.id, this.theatre_id, this.title, this.price, this.seats_available));
         getContext().getLog().info("Show details sent");
         return this;
     }
+
     private Behavior<Command> onAddBooking(AddBooking message) {
         final Http http = Http.get(getContext().getSystem());
-        //System.out.println("out if");
         getContext().getLog().info("out if");
-        Integer user_id=message.booking.user_id;
-        Integer show_id=message.booking.show_id;
-        Integer seats_booked=message.booking.seats_booked;
-        Integer amount=seats_booked*this.price;
-        if(seats_booked>-1 && seats_booked <= this.seats_available && WalletService.walletOperation(user_id,amount,"debit",http))
-        {
-            //System.out.println("in if");
+        Integer user_id = message.booking.user_id;
+        Integer show_id = message.booking.show_id;
+        Integer seats_booked = message.booking.seats_booked;
+        Integer amount = seats_booked * this.price;
+        if (seats_booked > -1 && seats_booked <= this.seats_available
+                && WalletService.walletOperation(user_id, amount, "debit", http)) {
             getContext().getLog().info("in if");
             count++;
-            this.seats_available=this.seats_available-seats_booked;
-            Booking newBooking =new Booking(count,user_id,show_id,seats_booked);
+            this.seats_available = this.seats_available - seats_booked;
+            Booking newBooking = new Booking(count, user_id, show_id, seats_booked);
             bookings.add(newBooking);
             message.replyTo.tell(newBooking);
-        }
-        else{
-            message.replyTo.tell(new Booking(-1,-1,-1,-1));
+        } else {
+            message.replyTo.tell(new Booking(-1, -1, -1, -1));
         }
         return this;
     }
@@ -110,11 +132,11 @@ public class ShowActor extends AbstractBehavior<ShowActor.Command>  {
     private Behavior<Command> onDeleteAllBookings(DeleteAllBookings message) {
         List<Booking> found = new ArrayList<ShowActor.Booking>();
         final Http http = Http.get(getContext().getSystem());
-        for(Booking booking : bookings){
-            Integer user_id=booking.user_id;
-            Integer amount=booking.seats_booked*this.price;
-            WalletService.walletOperation(user_id,amount,"credit",http);
-            seats_available+=booking.seats_booked;
+        for (Booking booking : bookings) {
+            Integer user_id = booking.user_id;
+            Integer amount = booking.seats_booked * this.price;
+            WalletService.walletOperation(user_id, amount, "credit", http);
+            seats_available += booking.seats_booked;
             found.add(booking);
         }
         bookings.removeAll(found);
@@ -126,18 +148,18 @@ public class ShowActor extends AbstractBehavior<ShowActor.Command>  {
         List<Booking> found = new ArrayList<ShowActor.Booking>();
         final Http http = Http.get(getContext().getSystem());
         boolean flag = false;
-        for(Booking booking : bookings){
-            if(Objects.equals(booking.user_id, message.user_id)){
-                seats_available+=booking.seats_booked;
-                Integer user_id=booking.user_id;
-                Integer amount=booking.seats_booked*this.price;
-                WalletService.walletOperation(user_id,amount,"credit",http);
+        for (Booking booking : bookings) {
+            if (Objects.equals(booking.user_id, message.user_id)) {
+                seats_available += booking.seats_booked;
+                Integer user_id = booking.user_id;
+                Integer amount = booking.seats_booked * this.price;
+                WalletService.walletOperation(user_id, amount, "credit", http);
                 found.add(booking);
                 flag = true;
             }
         }
         bookings.removeAll(found);
-        if(message.replyTo != null)
+        if (message.replyTo != null)
             message.replyTo().tell(new Response(flag));
         else
             message.workerReply().tell(new WorkerActor.Response(flag));
@@ -145,8 +167,11 @@ public class ShowActor extends AbstractBehavior<ShowActor.Command>  {
     }
 
     private Behavior<Command> onGetTheatre(GetTheatre message) {
-        boolean status=(Objects.equals(message.theatre_id, this.theatre_id));
-        message.workerRef().tell(new WorkerActor.TheatreOfShow(status,new Show(this.id,this.theatre_id,this.title,this.price,this.seats_available),message.i,message.replyTo));
+        boolean status = (Objects.equals(message.theatre_id, this.theatre_id));
+        message.workerRef()
+                .tell(new WorkerActor.TheatreOfShow(status,
+                        new Show(this.id, this.theatre_id, this.title, this.price, this.seats_available), message.i,
+                        message.replyTo));
         return this;
     }
 
--- Project-2-Phase-2/bookingService/src/main/java/com/example/WorkerActor.java
+++ Project-2-Phase-2/bookingService/src/main/java/com/example/WorkerActor.java
@@ -14,9 +14,8 @@ import java.util.*;
 
 import com.example.CborSerializable;
 
-
-public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
-    public Map<Integer,EntityRef<ShowActor.Command>> showActors;
+public class WorkerActor extends AbstractBehavior<WorkerActor.Command> {
+    public Map<Integer, EntityRef<ShowActor.Command>> showActors;
     public Integer user_id;
     public ActorRef<BookingRegistry.Bookings> replyTo;
     public ActorRef<ShowActor.Response> replyToDelete;
@@ -26,41 +25,57 @@ public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
     public Integer numProcessed;
     public boolean flag;
     List<BookingRegistry.Booking> userBookings = new ArrayList<>();
-    List<ShowActor.Show> showOfTheatre =new ArrayList<>();
+    List<ShowActor.Show> showOfTheatre = new ArrayList<>();
 
-    sealed interface Command extends CborSerializable{}
-    public final record GetUserBookings(ActorRef<BookingRegistry.Bookings> replyTo, Integer user_id) implements Command{}
-    public final record DeleteAllUserBookings(ActorRef<ShowActor.Response> replyTo, Integer user_id) implements Command{}
+    sealed interface Command extends CborSerializable {
+    }
 
-    public final record DeleteAllBookings(ActorRef<WorkerActor.DeleteResponse> replyTo) implements Command{}
+    public final record GetUserBookings(ActorRef<BookingRegistry.Bookings> replyTo, Integer user_id)
+            implements Command {
+    }
 
-    public final record GetShowBookings(List<ShowActor.Booking> bookings) implements Command{}
+    public final record DeleteAllUserBookings(ActorRef<ShowActor.Response> replyTo, Integer user_id)
+            implements Command {
+    }
 
-    public final record Booking(Integer id,Integer user_id,Integer show_id , Integer seats_booked)implements CborSerializable{}
-    public final record Bookings(List<ShowActor.Booking> bookings)implements CborSerializable{}
-    public final record Showlist(List<ShowActor.Show> showlist)implements CborSerializable{}
+    public final record DeleteAllBookings(ActorRef<WorkerActor.DeleteResponse> replyTo) implements Command {
+    }
 
-    public final record Response(boolean flag) implements Command{}
+    public final record GetShowBookings(List<ShowActor.Booking> bookings) implements Command {
+    }
 
-    public final record DeleteResponse(boolean flag) implements Command{}
+    public final record Booking(Integer id, Integer user_id, Integer show_id, Integer seats_booked)
+            implements CborSerializable {
+    }
 
+    public final record Bookings(List<ShowActor.Booking> bookings) implements CborSerializable {
+    }
 
+    public final record Showlist(List<ShowActor.Show> showlist) implements CborSerializable {
+    }
 
-    public final record ShowByTheatre(Integer theatre_id,ActorRef<Showlist> replyTo) implements Command{}
-    public final record TheatreOfShow(Boolean status,ShowActor.Show showDetails,Integer i,ActorRef<Showlist> replyTo) implements Command{}
+    public final record Response(boolean flag) implements Command {
+    }
 
-    
+    public final record DeleteResponse(boolean flag) implements Command {
+    }
 
-    //private WorkerActor(ActorContext<Command> context, Map<Integer,ActorRef<ShowActor.Command>> showActors) {
-    private WorkerActor(ActorContext<Command> context,Map<Integer,EntityRef<ShowActor.Command>> showActors) {
+    public final record ShowByTheatre(Integer theatre_id, ActorRef<Showlist> replyTo) implements Command {
+    }
+
+    public final record TheatreOfShow(Boolean status, ShowActor.Show showDetails, Integer i, ActorRef<Showlist> replyTo)
+            implements Command {
+    }
+
+    private WorkerActor(ActorContext<Command> context, Map<Integer, EntityRef<ShowActor.Command>> showActors) {
         super(context);
         this.numProcessed = 1;
         this.showActors = showActors;
-        this.selfRef = getContext().getSelf(); 
+        this.selfRef = getContext().getSelf();
     }
 
-    public static Behavior<Command> create(Map<Integer,EntityRef<ShowActor.Command>> showActors) {
-        return Behaviors.setup(context->new WorkerActor(context,showActors));
+    public static Behavior<Command> create(Map<Integer, EntityRef<ShowActor.Command>> showActors) {
+        return Behaviors.setup(context -> new WorkerActor(context, showActors));
     }
 
     @Override
@@ -68,9 +83,9 @@ public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
         return newReceiveBuilder()
                 .onMessage(GetShowBookings.class, this::onGetShowBookings)
                 .onMessage(GetUserBookings.class, this::onGetUserBookings)
-                .onMessage(ShowByTheatre.class,this::onShowByTheatre)
-                .onMessage(TheatreOfShow.class,this::onTheatreOfShow)
-                .onMessage(Response.class,this::onGetResponse)
+                .onMessage(ShowByTheatre.class, this::onShowByTheatre)
+                .onMessage(TheatreOfShow.class, this::onTheatreOfShow)
+                .onMessage(Response.class, this::onGetResponse)
                 .onMessage(DeleteAllUserBookings.class, this::onDeleteAllUserBookings)
                 .onMessage(DeleteAllBookings.class, this::onDeleteAllBookings)
                 .onMessage(DeleteResponse.class, this::onGetDeleteResponse)
@@ -82,17 +97,15 @@ public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
         for (ShowActor.Booking b : message.bookings) {
             this.userBookings.add(new BookingRegistry.Booking(b.id(), b.user_id(), b.show_id(), b.seats_booked()));
         }
-        if(this.numProcessed == 21){
+        if (this.numProcessed == 21) {
             List<BookingRegistry.Booking> result = new ArrayList<>();
-            for (BookingRegistry.Booking b : this.userBookings){
-                if(b.user_id() == this.user_id)
+            for (BookingRegistry.Booking b : this.userBookings) {
+                if (b.user_id() == this.user_id)
                     result.add(b);
             }
             this.replyTo.tell(new BookingRegistry.Bookings(Collections.unmodifiableList(new ArrayList<>(result))));
-            //return Behaviors.stopped();
             return this;
-        }
-        else{
+        } else {
             EntityRef<ShowActor.Command> ref = showActors.get(numProcessed);
             ref.tell(new ShowActor.GetBookings(this.selfRef));
         }
@@ -107,38 +120,35 @@ public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
         return this;
     }
 
-
     private Behavior<Command> onTheatreOfShow(TheatreOfShow message) {
-        if(message.status)
+        if (message.status)
             this.showOfTheatre.add(message.showDetails);
-        if(message.i == 0){
+        if (message.i == 0) {
             message.replyTo().tell(new Showlist(showOfTheatre));
-            //return Behaviors.stopped();
             return this;
         }
         return this;
     }
 
     private Behavior<Command> onShowByTheatre(ShowByTheatre message) {
-        int i=20;
-        System.out.println("WorkerActor: onShowByTheatre"+showActors.get(1));
-        for (int k=1;k<=20;k++) {
+        int i = 20;
+        System.out.println("WorkerActor: onShowByTheatre" + showActors.get(1));
+        for (int k = 1; k <= 20; k++) {
             EntityRef<ShowActor.Command> ref = showActors.get(1);
-           ref.tell(new ShowActor.GetTheatre(this.selfRef, --i, message.theatre_id,message.replyTo));
+            ref.tell(new ShowActor.GetTheatre(this.selfRef, --i, message.theatre_id, message.replyTo));
         }
         return this;
     }
 
-
     private Behavior<Command> onGetResponse(Response message) {
         this.numProcessed += 1;
-        if(message.flag())
+        if (message.flag())
             this.flag = true;
 
-        if(this.numProcessed == 21){
+        if (this.numProcessed == 21) {
             this.replyToDelete.tell(new ShowActor.Response(this.flag));
             this.numProcessed = 1;
-            this.flag=  false;
+            this.flag = false;
             return this;
         }
         return this;
@@ -146,35 +156,32 @@ public class WorkerActor extends AbstractBehavior<WorkerActor.Command>  {
 
     private Behavior<Command> onDeleteAllUserBookings(DeleteAllUserBookings message) {
         this.replyToDelete = message.replyTo;
-            EntityRef<ShowActor.Command> ref = showActors.get(1);
-            ref.tell(new ShowActor.DeleteUserBooking(message.user_id, 1, null, this.selfRef));
+        EntityRef<ShowActor.Command> ref = showActors.get(1);
+        ref.tell(new ShowActor.DeleteUserBooking(message.user_id, 1, null, this.selfRef));
         return this;
     }
 
-
     private Behavior<Command> onGetDeleteResponse(DeleteResponse message) {
         this.numProcessed += 1;
-        if(message.flag()){
+        if (message.flag()) {
             this.flag = true;
         }
-        
 
-        if(this.numProcessed == 21){
+        if (this.numProcessed == 21) {
             this.replyToDeleteAll.tell(new DeleteResponse(this.flag));
             this.numProcessed = 1;
             this.flag = false;
             return this;
-        }
-        else {
-        EntityRef<ShowActor.Command> ref = showActors.get(numProcessed);
-        ref.tell(new ShowActor.DeleteAllBookings(this.selfRef));
+        } else {
+            EntityRef<ShowActor.Command> ref = showActors.get(numProcessed);
+            ref.tell(new ShowActor.DeleteAllBookings(this.selfRef));
         }
         return this;
     }
 
     private Behavior<Command> onDeleteAllBookings(DeleteAllBookings message) {
         this.replyToDeleteAll = message.replyTo;
-        getContext().getLog().info("Show id: "+user_id);
+        getContext().getLog().info("Show id: " + user_id);
         EntityRef<ShowActor.Command> ref = showActors.get(1);
         ref.tell(new ShowActor.DeleteAllBookings(this.selfRef));
 
